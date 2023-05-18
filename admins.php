fabeb43ee09906ff5fdb892c76554aa189a7345a
ria-kee <mariakarlaaa.r@gmail.com>
2023-05-18 14:45:19 +0800

    changes for admins

--- admins.php
+++ admins.php
@@ -4,42 +4,60 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
 
-<div class="grid-container">
-    <div class="grid-item">
-        <h3>What are you looking for?</h3>
-    </div>
+<div class="body-container">
+    <div class="grid-container body-item">
+        <div class="grid-item">
+            <h3>What are you looking for?</h3>
+        </div>
 
-    <div class="grid-item">
-        <h3>Category</h3>
-    </div>
+        <div class="grid-item">
+            <h3>Category</h3>
+        </div>
 
-    <div class="grid-item">
+        <div class="grid-item">
 
-    </div>
+        </div>
 
-    <div class="grid-item">
-        <div class="search">
-        <span class="material-symbols-rounded">search</span>
+        <div class="grid-item">
+            <div class="search">
+            <span class="material-symbols-rounded">search</span>
 
-        <input type="text" class="searchField" placeholder="Search for name, id, or etc.">
+            <input type="text" class="searchField" placeholder="Search for name, id, or etc.">
+            </div>
+        </div>
+
+        <div class="grid-item">
+            <div class="dropdown">
+            <select class="category">
+                <option value="option1" selected>All</option>
+                <option value="option2">Name</option>
+                <option value="option3">Email</option>
+                <option value="option3" >Department</option>
+            </select>
+            </div>
+        </div>
+
+        <div class="grid-item">
+            <button>Search</button>
         </div>
     </div>
+    <div class="button-container body-item">
+        <div class="grid-item">
+            <h3>Actions</h3>
+        </div>
 
-    <div class="grid-item">
-        <div class="dropdown">
-        <select class="category">
-            <option value="option1" selected>All</option>
-            <option value="option2">Name</option>
-            <option value="option3">Email</option>
-            <option value="option3" >Department</option>
-        </select>
+        <div class="grid-tem"></div>
+
+        <div class="grid-item button1">
+            <span class="material-symbols-rounded">add</span>
+            <button>Add Admin</button>
+        </div>
+
+        <div class="grid-item button2">
+            <span class="material-symbols-rounded">visibility</span>
+            <button>View Logs</button>
         </div>
     </div>
-
-    <div class="grid-item">
-        <button>Search</button>
-    </div>
-
 </div>
 
 
