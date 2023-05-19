6acde862a71664186680f442378eb67bf4119a16
ria-kee <mariakarlaaa.r@gmail.com>
2023-05-19 13:25:48 +0800

    minimal changes

--- employees.php
+++ employees.php
@@ -1,1 +1,69 @@
+
 <?php include_once "header/active_employees.php";?>
+<link rel="stylesheet" href="css/style_employees.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+
+
+<div class="whole-container">
+    <div class="body-container">
+            <div class="grid-container body-item">
+                <div class="grid-item">
+                    <h3>What are you looking for?</h3>
+                </div>
+
+                <div class="grid-item">
+                    <h3>Category</h3>
+                </div>
+
+                <div class="grid-item">
+
+                </div>
+
+                <div class="grid-item">
+                    <div class="search">
+                        <span class="material-symbols-rounded">search</span>
+
+                        <input type="text" class="searchField" placeholder="Search for name, id, or etc.">
+                    </div>
+                </div>
+
+                <div class="grid-item">
+                    <div class="dropdown">
+                        <select class="category">
+                            <option value="option1" selected>All</option>
+                            <option value="option2">Name</option>
+                            <option value="option3">Email</option>
+                            <option value="option3" >Department</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="grid-item">
+                    <button>Search</button>
+                </div>
+            </div>
+            <div class="button-container body-item">
+                <div class="grid-item">
+                    <h3>Actions</h3>
+                </div>
+
+                <div class="grid-tem"></div>
+
+                <div class="grid-item button1">
+                    <span class="material-symbols-rounded">person_add</span>
+                    <button>Add Employee</button>
+                </div>
+
+                <div class="grid-item button2">
+                    <span class="material-symbols-rounded">download</span>
+                    <button>Download Excel</button>
+                </div>
+            </div>
+        </div>
+<!--END OF TOP-->
+
+    <div class="table">
+
+    </div>
+</div>
--- header/active_employees.php
+++ header/active_employees.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employees</title>
     <link rel="icon" href="../assets/PACADA/PACADA.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
     <link rel="stylesheet" href="css/style_dashboard.css">
+
 </head>
 <body>
 <div class="container">
